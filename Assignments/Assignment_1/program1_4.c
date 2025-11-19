34d2d2c4b24a4bc3a9254a59c6e527272279bb41
SanketHajare44 <sankethajare44@gmail.com>
2025-11-19 10:39:57 +0530

    Add solution for Assignment_1 Q4

--- Assignments/Assignment_1/program1_4.c
+++ Assignments/Assignment_1/program1_4.c
@@ -1,11 +1,28 @@
 // Accept one number and check whether is divisible by 5 or not.
 
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 #include<stdbool.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : Check
+//  Description :   It is used Display number is divisible by 5 or not 
+//  Input :         
+//  Output :        void
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          16/10/2025
+//
+/////////////////////////////////////////////////////////////////
+
 bool Check(int iNo)
 {
-    if((iNo%5) == 0)
+    if((iNo%5) == 0)                    // Business logic
     {
         return true;
     }
@@ -13,17 +30,30 @@ bool Check(int iNo)
     {
         return false;
     }
-}
+}// End Check Function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(1)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
 
 int main()
 {
-    int iValue = 0;
-    bool bRet = false;
+    int iValue = 0;                     // To accept user input
+    bool bRet = false;                  // To store the result
 
     printf("Enter number :\n");
     scanf("%d",&iValue);
 
-    bRet = Check(iValue);
+    bRet = Check(iValue);               // Function call
 
     if(bRet == true)
     {
@@ -35,4 +65,13 @@ int main()
     }
 
     return 0;
-}
+}// End main function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  Input : 100   Output : Divisible by 5
+//  Input : 13    Output : Not Divisible by 5
+//
+/////////////////////////////////////////////////////////////////
