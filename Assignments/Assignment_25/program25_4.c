94ac057534ccedc8d990b3cae832165d146991aa
SanketHajare44 <sankethajare44@gmail.com>
2025-11-22 00:10:08 +0530

    Add solution for Assignment_25 Q4

---
+++ Assignments/Assignment_25/program25_4.c
@@ -0,0 +1,89 @@
+// Accept N numbers from user and display all such elements which are even and divisible by 3 and 5.
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : Display
+//  Description :   It is use to display all such elements which are even and divisible by 5..
+//  Input :         Integer array  Integer 
+//  Output :        void
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          16/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+void Display(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if((Arr[iCnt]%3 == 0) && (Arr[iCnt]%5 == 0))    // Business logic
+        {
+           printf("%d\t",Arr[iCnt]);
+        }        
+    }
+    printf("\n");
+
+}// End of Display function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(N)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{   
+    int iCnt = 0; 
+    int iLength = 0;
+
+    int *ptr = NULL;
+    
+    printf("Enter number of elements :\n");
+    scanf("%d",&iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+
+    if(NULL == ptr)
+    {
+        printf("Unable to allocate the memory \n");
+        return -1;
+    }
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {   
+        printf("Enter %d elements \n",iCnt+1);
+        scanf("%d",&ptr[iCnt]);
+    }
+
+    Display(ptr, iLength);                              // Function call
+
+    free(ptr);
+    
+    return 0;
+
+}// End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  Input : 6   Input : 85 66 3 15 93 88   Output : 15
+//  Input : 6   Input : 44 30 93 15 70 10  Output : 30 10
+//
+/////////////////////////////////////////////////////////////////
