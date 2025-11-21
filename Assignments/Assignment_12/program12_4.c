9e07f6eaa44393803951a4bae60c3284a39c1678
SanketHajare44 <sankethajare44@gmail.com>
2025-11-21 10:43:02 +0530

    Add solution for Assignment_12 Q4

---
+++ Assignments/Assignment_12/program12_4.c
@@ -0,0 +1,82 @@
+// Sum of all factors of a number
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : Sum_of_Even_Factors
+//  Description :   It is used to display addition of even Factors
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          29/10/2025
+//
+/////////////////////////////////////////////////////////////////
+
+int Sum_of_Even_Factors(
+                         int number             // Input
+                       )
+{
+    int iCnt = 0;                               // Loop counter
+    int iSum = 0;                               // To store the result
+
+    if(number < 0)                              // Updater
+    {
+        number = -number;
+    }
+
+    for(iCnt = 1; iCnt <= (number/2); iCnt++)   // Business logic
+    {
+        if((number%iCnt) == 0 && (iCnt%2) == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    printf("\n");
+
+    return iSum;
+}// End of Sum_of_Even_Factors function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(N/2)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{   
+    int iValue = 0;                             // To accept user input
+    int iRet = 0;                               // To store the result
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+    
+    iRet = Sum_of_Even_Factors(iValue);         // Function call
+
+    printf("Addition of all even factor is : %d\n",iRet);
+
+    return 0;
+
+}// End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  Input : 10      Output : 2
+//  Input : 23      Output : 0
+//  Input : -18     Output : 8
+//
+/////////////////////////////////////////////////////////////////
