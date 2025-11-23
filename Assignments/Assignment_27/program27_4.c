cab9984fd563cd8d7aa3d55dd92807f5a2d2eb25
SanketHajare44 <sankethajare44@gmail.com>
2025-11-24 01:38:03 +0530

    Add solution for Assignment_27 Q4

---
+++ Assignments/Assignment_27/program27_4.c
@@ -0,0 +1,97 @@
+/*  
+    Accept number from user and Display below pattern
+
+    Input1 : 4      Input2 : 4
+
+    Output :    *   #   *   #
+                *   #   *   #
+                *   #   *   #
+                *   #   *   #
+*/
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : pattern
+//  Description :   It is use to display Pattern
+//  Input :         Integer , Integer 
+//  Output :        void
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          23/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j % 2 == 0)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+        
+}// End of Pattern function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(N)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{   
+    int iValue1 = 0, iValue2 = 0;                             // To accept user input
+    
+    printf("Enter the number of Rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of Columns :\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);                            // Function call
+    
+    return 0;
+
+}// End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  
+//  Input : 4 , 5
+// 
+// Output: 
+// *	#	*	#	*	
+// *	#	*	#	*	
+// *	#	*	#	*	
+// *	#	*	#	*	
+// *	#	*	#	*
+//
+/////////////////////////////////////////////////////////////////
