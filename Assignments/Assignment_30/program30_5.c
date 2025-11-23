ddef10d1681c907ade0e10b03886780995ea7912
SanketHajare44 <sankethajare44@gmail.com>
2025-11-24 01:43:29 +0530

    Add solution for Assignment_30 Q5

---
+++ Assignments/Assignment_30/program30_5.c
@@ -0,0 +1,100 @@
+/*
+    iRow : 4
+    iCol : 4
+
+            1	2	3	4	
+            1	*	*	4	
+            1	*	*	4	
+            1	2	3	4
+
+*/
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : pattern
+//  Description :   It is use to display Pattern
+//  Input :         Integer , Integer 
+//  Output :        void
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          23/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    for(int i = 1; i <= iRow; i++)
+    {
+        for(int j = 1; j <= iCol; j++)
+        {
+            if(i == 1 || i == iRow)
+            {
+                printf("%d\t", j);
+            }                
+            else if(j == 1)
+            {
+                printf("1\t");
+            }                
+            else if(j == iCol)
+            {
+                printf("%d\t", j);
+            }            
+            else
+            {
+                printf("*\t");
+            }
+                
+        }
+        printf("\n");
+    }
+}// End of Pattern function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(N)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{   
+    int iValue1 = 0, iValue2 = 0;                         // To accept user input
+    
+    printf("Enter the number of Rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of Columns :\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);                            // Function call
+    
+    return 0;
+
+}// End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  
+// Input  : 5 7
+// Output :
+//          1	2	3	4	5	6	7
+//          1	*	*	*	*	*	7
+//          1	*	*	*	*	*	7
+//          1	*	*	*	*	*	7
+//          1	2	3	4	5	6	7
+//
+/////////////////////////////////////////////////////////////////
