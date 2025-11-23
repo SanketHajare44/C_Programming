9c27cad68b6c321ce5668d67aeabfd201e03b6fc
SanketHajare44 <sankethajare44@gmail.com>
2025-11-24 01:39:55 +0530

    Add solution for Assignment_28 Q5

---
+++ Assignments/Assignment_28/program28_5.c
@@ -0,0 +1,89 @@
+/*  
+    Accept number from user and Display below pattern
+
+    Input1 : 3      Input2 : 4
+
+    Output :    1   2   3   4
+                5   6   7   8
+                9   10  11  12
+*/
+
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : pattern
+//  Description :   It is use to display Pattern
+//  Input :         Integer , Integer 
+//  Output :        void
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          23/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    int iNo = 1;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%d\t", iNo);
+            iNo++;
+        }
+        printf("\n");
+    }
+
+}// End of Pattern function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(N)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{   
+    int iValue1 = 0, iValue2 = 0;                         // To accept user input
+    
+    printf("Enter the number of Rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of Columns :\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);                            // Function call
+    
+    return 0;
+
+}// End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  
+//  Input  : 4   3
+//  Output :  
+//            1   2   3
+//            4   5   6
+//            7   8   9
+//           10  11  12
+//
+/////////////////////////////////////////////////////////////////
