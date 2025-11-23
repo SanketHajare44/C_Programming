e8a3547e13d48903dd545c7115557812b8579ce7
SanketHajare44 <sankethajare44@gmail.com>
2025-11-24 01:35:48 +0530

    Add solution for Assignment_26 Q2

---
+++ Assignments/Assignment_26/program26_2.c
@@ -0,0 +1,73 @@
+/*  
+    Accept number from user and Display below pattern
+
+    Input : 5
+    Output : 5  #   4   #   3   #   2   #   1   #
+*/
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : pattern
+//  Description :   It is use to display Pattern
+//  Input :         Integer
+//  Output :        void
+//  Author :        Sanket Sadashiv Hajare
+//  Date :          23/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+
+        printf("%d\t""#\t",iCnt);
+
+    }
+    printf("\n");
+
+}// End of Pattern function
+
+/////////////////////////////////////////////////////////////////
+//
+//  Time Complexity : O(N)
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{   
+    int iValue = 0;                             // To accept user input
+    
+    printf("Enter the number :\n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);                            // Function call
+    
+    return 0;
+
+}// End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test cases succesfully handled by the application
+//
+//  Input : 5        Output : 5  #   4   #   3   #   2   #   1   #
+//  Input : 7        Output : 7	#	6	#	5	#	4	#	3	#	2	#	1	#
+//
+/////////////////////////////////////////////////////////////////
