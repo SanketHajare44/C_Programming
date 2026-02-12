07847399f63f1698b06493e092ae868b4273f9f9
SanketHajare44 <sankethajare44@gmail.com>
2026-02-12 12:57:34 +0530

    Add solution for Assignment 68 Q1

---
+++ Assignments/Assignment_68/program68_1.c
@@ -0,0 +1,25 @@
+/*
+    Write a recursive program which display below pattern
+    *   *   *   *   *
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+    static int i = 0;
+
+    if(i <= 5)
+    {
+        printf("*\t");
+        i++;
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
