1cfa6f2f6be934b98246d66c5271739c8f1f6cf1
SanketHajare44 <sankethajare44@gmail.com>
2026-02-13 09:52:36 +0530

    problems on Recursion

---
+++ Assignments/Assignment_69/program69_3.c
@@ -0,0 +1,30 @@
+/*
+    Write a recursive program which display below pattern
+    Input : 5
+    5   4   3   2   1
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo >= 1)
+    {
+        printf("%d\t",iNo);
+        Display(iNo - 1); 
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    printf("\n");
+
+    return 0;
+}
