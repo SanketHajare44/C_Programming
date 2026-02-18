4d4babab8df28a3c7e22c7a5301a5fba368549be
SanketHajare44 <sankethajare44@gmail.com>
2026-02-19 00:20:28 +0530

    Recursive problem

---
+++ Assignments/Assignment_39/program39_5.c
@@ -0,0 +1,28 @@
+/*
+    write a recursive program which display pbelow pattern on screen
+    Input : 5
+    Output : A  B   C   D   E   F
+*/
+
+#include<stdio.h>
+
+void Display(int No)
+{
+    static char cValue = 'a';
+
+    if(No >= 1)
+    {
+        printf("%c\t",cValue);
+        cValue++;
+
+        Display(No - 1);
+    }
+}
+
+int main()
+{
+    Display(5);
+
+    return 0;
+}
+
