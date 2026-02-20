2c2b646f98fd022ee252d268f9c49257f354d158
SanketHajare44 <sankethajare44@gmail.com>
2026-02-20 07:03:02 +0530

    recrsion problem

---
+++ Assignments/Assignment_41/program41_4.c
@@ -0,0 +1,36 @@
+/*
+    Write a recursive program which accept input from user and its retunrs factorial 
+    Input : 5
+    Output : 120
+*/
+
+#include<stdio.h>
+
+int Display(int iNo)
+{
+    if(iNo < 0)
+    {
+        return 0;
+    }
+
+    if(iNo == 0)
+    {
+        return 1;
+    }
+
+    return iNo * Display(iNo - 1);
+}
+
+int main()
+{
+    int iRet = 0;
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    iRet = Display(iValue);
+    printf("Factorial is  : %d\n",iRet);
+
+    return 0;
+}
