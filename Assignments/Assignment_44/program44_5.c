32cbbfb5aa6f0218993b64cc58e4a5cdb2cfbace
SanketHajare44 <sankethajare44@gmail.com>
2026-02-24 17:20:45 +0530

    problems on Singly Linear LinkedList

---
+++ Assignments/Assignment_44/program44_5.c
@@ -0,0 +1,82 @@
+// Display all even numbers
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE; 
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else 
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = first;
+
+    while(temp != NULL)
+    {
+        printf("| %d |->",temp->data);
+        temp = temp->next;
+    }
+
+    printf("| NULL |\n");
+}
+
+void EvenDisplay(PNODE first)
+{
+    PNODE temp = NULL;
+    temp = first;
+
+    while(temp != NULL)
+    {
+        if(temp->data % 2 == 0)
+        {
+            printf("%d\t",temp->data);
+        }
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 55);
+    InsertFirst(&head, 23);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 16);
+
+    Display(head);
+
+    EvenDisplay(head);
+
+    return 0;
+}
