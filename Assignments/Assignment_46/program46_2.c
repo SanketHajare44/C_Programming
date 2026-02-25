3f4cd07d2efc769816c436c3cf46e4660d25c5eb
SanketHajare44 <sankethajare44@gmail.com>
2026-02-25 22:32:05 +0530

    Problems on Linked List

---
+++ Assignments/Assignment_46/program46_2.c
@@ -0,0 +1,82 @@
+/*
+    Display Elements Leass than x
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE; 
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else 
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = first;
+
+    while(temp != NULL)
+    {
+        printf("| %d |->",temp->data);
+        temp = temp->next;
+    }
+
+    printf("| NULL |\n");
+}
+
+void DisplayLess(PNODE first, int iNo)
+{
+    PNODE temp = NULL;
+    temp = first;
+
+    while(temp != NULL)
+    {
+        if(temp->data < iNo)
+        {
+            printf("%d\t",temp->data);
+        }
+
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InsertFirst(&head,10);
+    InsertFirst(&head,16);
+    InsertFirst(&head,102);
+    InsertFirst(&head,45);
+    InsertFirst(&head,35);
+
+    Display(head);
+
+    DisplayLess(head,35);
+
+    return 0;
+}
