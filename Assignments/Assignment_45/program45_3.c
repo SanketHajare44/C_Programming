0231422dc736a856999403b4c24e6948d79f2949
SanketHajare44 <sankethajare44@gmail.com>
2026-02-25 22:30:37 +0530

    Problems on Linked List

---
+++ Assignments/Assignment_45/program45_3.c
@@ -0,0 +1,88 @@
+/*
+    Last occurence position
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE; 
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else 
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = first;
+
+    while(temp != NULL)
+    {
+        printf("| %d |->",temp->data);
+        temp = temp->next;
+    }
+
+    printf("| NULL |\n");
+}
+
+int LastOccure(PNODE first,int iNo)
+{
+    PNODE temp = NULL;
+    temp = first;
+    int pos = 1;
+    int LastPos = -1;
+
+    while(temp != NULL)
+    {
+        if(temp->data == iNo)
+        {
+            LastPos = pos;   
+        }
+        temp = temp->next;
+        pos++;
+    }
+
+    return LastPos;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 50);
+    InsertFirst(&head, 10);
+
+    Display(head);
+
+    iRet = LastOccure(head,11);
+
+    printf("%d\n",iRet);
+
+    return 0;
+}
