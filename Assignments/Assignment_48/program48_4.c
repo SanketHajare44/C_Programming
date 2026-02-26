35f176b6903c2300c3fa73067b1ac3940b7540f9
SanketHajare44 <sankethajare44@gmail.com>
2026-02-26 09:33:52 +0530

    Problems on Linked List

---
+++ Assignments/Assignment_48/program48_4.c
@@ -0,0 +1,87 @@
+/*
+   Display elements at Even positions
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE; 
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else 
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = first;
+
+    while(temp != NULL)
+    {
+        printf("| %d |->",temp->data);
+        temp = temp->next;
+    }
+
+    printf("| NULL |\n");
+}
+
+void DisplayEvenPosition(PNODE first)
+{
+    PNODE temp = NULL;
+    temp = first;
+    int pos = 0;
+
+    while(temp != NULL)
+    {   
+        pos++;
+
+        if(pos % 2 == 0)
+        {
+            printf("%d \t",temp->data);
+        }
+    
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,10);
+    InsertFirst(&head,2);
+    InsertFirst(&head,102);
+    InsertFirst(&head,7);
+    InsertFirst(&head,35);
+
+    Display(head);
+    
+    DisplayEvenPosition(head);
+
+    return 0;
+}
